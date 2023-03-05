4cac678d8a3408d0c1122c57187bc2192f6554d4
Bernardo Espinoza <bernardo.julian.espinoza@gmail.com>
2023-03-05 19:24:51 -0300

    First commit singleton ts

---
+++ CS Singleton thread safe/Program.cs
@@ -0,0 +1,58 @@
+﻿using SL.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Singleton_thread_safe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //USO DE LOGGERS: EXCEPCIONES - REGLAS DE NEGOCIO - LOGIN/LOGOUT ETC.
+
+            ////////////////CON SINGLETON THREAD SAFE
+
+            LoggerManager instancia1 = LoggerManager.Current;
+            LoggerManager instancia2 = LoggerManager.Current;
+
+            if (instancia1 == instancia2)
+            {
+                Console.WriteLine("Esto es un singleton con thread safe");
+            }
+            LoggerManager.Current.Write("Logger por consola...con singleton thread safe", EventLevel.Informational);
+            Console.ReadLine();
+
+
+            /*
+
+            //////////////CON SINGLETON
+            
+            LoggerManager instancia1 = LoggerManager.GetInstance();
+            LoggerManager instancia2 = LoggerManager.GetInstance();
+
+            if (instancia1 == instancia2)
+            {
+                Console.WriteLine("Esto es un singleton");
+            }
+            Console.ReadLine();
+
+
+            //////////////SIN SINGLETON
+            
+            LoggerManager loggerManager = new LoggerManager();
+
+            loggerManager.Write("Logger por consola...", EventLevel.Informational);
+
+            Console.WriteLine("Terminado");
+
+            */
+
+
+        }
+    }
+}
---
+++ SL/Services/LoggerManager.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics.Tracing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace SL.Services
+{
+    public sealed class LoggerManager
+    {
+
+        ///PARAMETRIZADO DE LOGGERS CON SINGLETON THREAD SAFE
+
+        private string filePath;
+
+        private readonly static LoggerManager loggerManager = new LoggerManager();
+
+        private LoggerManager()
+        {
+            //PARAMETRIZACION
+            filePath = ConfigurationManager.AppSettings["filePathLogger"];
+        }
+
+        public static LoggerManager Current
+        {
+            get
+            {
+                return loggerManager;
+            }
+        }
+
+
+
+
+        ///PUEDE REALIZARSE TAMBIEN USANDO MUTEX
+
+        /*
+
+            private string filePath;
+
+            private static LoggerManager loggerManager = null;
+
+            private static object mutex = new object();
+
+            private LoggerManager()
+            {
+                //PARAMETRIZACION
+                filePath = ConfigurationManager.AppSettings["filePathLogger"];
+            }
+
+            public static LoggerManager GetInstance()
+
+            {
+
+
+                if (loggerManager == null)
+                {
+
+
+                    lock (mutex)
+                    {
+                        if (loggerManager == null)
+                        {
+                            loggerManager = new LoggerManager();
+                        }
+
+                    }
+
+                }
+
+                return loggerManager;
+
+        */
+
+
+
+        /* 
+
+            /// PARAMETRIZADO DE LOGGERS CON SINGLETON 
+
+
+
+            private string filePath;
+
+            private static LoggerManager loggerManager = null;
+
+            private LoggerManager()
+            {
+                //PARAMETRIZACION
+                filePath = ConfigurationManager.AppSettings["filePathLogger"];
+            }
+
+           public static LoggerManager GetInstance()
+
+            {
+                if (loggerManager==null)
+                {
+                    loggerManager = new LoggerManager();
+                }
+
+                return loggerManager;
+            }
+
+
+
+
+
+        /// PARAMETRIZADO DE LOGGERS SIN SINGLETON 
+
+
+        private string filePath;
+
+        public LoggerManager()
+        {
+            filePath = ConfigurationManager.AppSettings["filePathLogger"];
+        }
+
+
+    */
+        public void Write(string message, EventLevel eventLevel)
+        {
+
+
+            using (StreamWriter streamWriter = new StreamWriter(filePath, true))
+            {
+
+                streamWriter.WriteLine($"{DateTime.Now.ToString("dd.MM.yy hh:mm:ss")} [Severity {eventLevel.ToString()}]:{message}");
+
+
+            }
+        }
+
+    }
+
+}
